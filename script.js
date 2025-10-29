ad24556193547e0295be87407b74a6b2913cc40b
SREYA4358 <sreyapunna4358@gmail.com>
2025-10-29 22:39:02 +0530

    added color

---
+++ script.js
@@ -0,0 +1,3 @@
+<img src="./sir-simo-crUoLUUD0Ew-unsplash.jpg"  width="600px" height="400px">
+    <p>This project uses a combination of computer vision, AI, and classical algorithms to ensure fairness, authentication, and security. The most critical parts rely on deep learning for face recognition and anomaly detection, plagiarism detection for text, and real-time monitoring with OpenCV.</p>
+    
